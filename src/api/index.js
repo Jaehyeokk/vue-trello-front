42169ad834ba58d2f4543db0b5768844424513cd
Jaehyeokk <lionjk92@naver.com>
2021-11-09 16:21:29 +0900

    Creating card

--- src/api/index.js
+++ src/api/index.js
@@ -24,6 +24,12 @@ export const setAuthInHeader = token => {
   axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {email, password}) 
+  }
+}
+
 export const board = {
   fetch(id) {
     return id ? request('get', `/boards/${id}`) : request('get', '/boards')
@@ -32,8 +38,9 @@ export const board = {
     return request('post', '/boards', {title})
   }
 }
-export const auth = {
-  login(email, password) {
-    return request('post', '/login', {email, password}) 
+
+export const card = {
+  create(title, listId, pos) {
+    return request('post', '/cards', {title, listId, pos})
   }
 }
