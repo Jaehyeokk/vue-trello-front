1c8c017e83d33e1aa9cd8b680c0d914076ccc140
Jaehyeokk <lionjk92@naver.com>
2021-11-24 23:14:21 +0900

    리스트 수정 기능 구현

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="list">
     <div class="list-header">
-      <div class="list-header-title">{{data.title}}</div>
+      <input v-if="isEditTitle" v-model="inputTitle" class="form-control input-title" type="text" ref="inputTitle" @blur="onBlurTitle" @keyup.enter="onSubmitTitle">
+      <div v-else class="list-header-title" @click="onClickTitle">{{data.title}}</div>
     </div>
     <div class="card-list">
       <CardItem v-for="card in data.cards" :key="card.id" :data="card" />
@@ -18,6 +19,7 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
 import AddCard from './AddCard.vue';
 import CardItem from './CardItem.vue'
 
@@ -29,9 +31,35 @@ export default {
   props: ['data'],
   data() {
     return {
-      isAddCard: false
+      isAddCard: false,
+      isEditTitle: false,
+      inputTitle: '',
     }
   },
+  created() {
+    this.inputTitle = this.data.title
+  },
+  methods: {
+    ...mapActions([
+      'UPDATE_LIST'
+    ]),
+    onClickTitle() {
+      this.isEditTitle = true
+      this.$nextTick(() => this.$refs.inputTitle.focus())
+    },
+    onBlurTitle() {
+      this.isEditTitle = false
+    },
+    onSubmitTitle() {
+      this.onBlurTitle()
+      this.inputTitle = this.inputTitle.trim()
+      if (!this.inputTitle) return
+      const id = this.data.id
+      const title = this.inputTitle
+      if (title === this.data.title) return
+      this.UPDATE_LIST({id, title})
+    }
+  }
 }
 </script>
 
