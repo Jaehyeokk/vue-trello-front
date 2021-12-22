3d8e7f32b95f70db978fc0cb3b961bb5746943e9
Jaehyeokk <lionjk92@naver.com>
2021-12-22 19:52:21 +0900

    라우트 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import PageNotFound from '@/views/PageNotFound.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -8,6 +10,24 @@ const routes = [
 	{
 		path: '/',
 		component: Home,
+	},
+	{
+		path: '/login',
+		component: Login,
+	},
+	{
+		path: '/b/:bid',
+		component: () => import('@/views/Board.vue'),
+		children: [
+			{
+				path: 'c/:cid',
+				component: () => import('@/views/Card.vue'),
+			},
+		],
+	},
+	{
+		path: '*',
+		component: PageNotFound,
 	},
 ]
 
